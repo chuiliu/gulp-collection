f3100a0644541250c0ca89dd29fb6be0ffc8c9d4
chuiliu <zhengchuiliu@foxmail.com>
2017-01-11 16:43:49 +0800

    update

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -2,7 +2,8 @@
 
 var gulp = require('gulp'),
     browserSync = require('browser-sync').create(),
-    spritesmith = require('gulp.spritesmith'),
+    del = require('del'),
+    runSequence = require('run-sequence'),
     $ = require('gulp-load-plugins')();
 
 
@@ -11,27 +12,29 @@ gulp.task('styles', function() {
             'src/styles/reset.scss',
             'src/styles/style.scss'
         ])
-        .pipe(plumber())
-        .pipe(sass({
+        .pipe($.plumber())
+        .pipe($.sourcemaps.init())
+        .pipe($.sass({
             outputStyle: 'expanded',
             errLogToConsole: false,
             onError: function(err) {
-                return notify().write(err);
+                return $.notify().write(err);
             }
         }))
+        .pipe($.sourcemaps.write())
         .on('error', sass.logError)
-        .pipe(autoprefixer({
-            browsers: ['last 15 versions', 'Firefox >= 20', 'ie 8', 'ie 9'],
-            cascade: true,  // 自动对齐
-            remove: false   // 是否移除不必要的前缀
+        .pipe($.autoprefixer({
+            browsers: ['last 20 versions', 'Firefox >= 20', 'safari 5', 'opera 12.1', 'ie 8', 'ie 9', 'android 4','ios 6'],
+            cascade: true,
+            remove: true
         }))
         .pipe(gulp.dest('dist/assets/css'))
-        .pipe(rename({
+        .pipe($.rename({
             suffix: '.min'
         }))
-        .pipe(minifycss())
+        .pipe($.minifycss())
         .pipe(gulp.dest('dist/assets/css'))
-        .pipe(notify({
+        .pipe($.notify({
             message: 'styles task complete'
         }))
         .pipe(browserSync.stream());
@@ -40,60 +43,46 @@ gulp.task('styles', function() {
 
 gulp.task('scripts', function() {
     return gulp.src('src/scripts/**/*.js')
-        .pipe(jshint('.jshintrc'))
-        .pipe(jshint.reporter('default'))
-        .pipe(concat('index.js'))
+        .pipe($.jshint('.jshintrc'))
+        .pipe($.jshint.reporter('default'))
+        .pipe($.concat('index.js'))
         .pipe(gulp.dest('dist/assets/js'))
-        .pipe(rename({
+        .pipe($.rename({
             suffix: '.min'
         }))
-        .pipe(uglify())
+        .pipe($.uglify())
         .pipe(gulp.dest('dist/assets/js'))
-        .pipe(notify({
+        .pipe($.notify({
             message: 'scripts task complete'
         }));
 });
 
 
-// 图片压缩
 gulp.task('images', function() {
     return gulp.src('src/images/**/*')
-        .pipe($.cache($.imagemin({          // 修改或新增的图片会被压缩
+        .pipe($.cache($.imagemin({
             optimizationLevel: 3,
             progressive: true,
             interlaced: true
         })))
-        .pipe(gulp.dest('dist/assets/images'))
-        .pipe(notify({
+        .pipe(gulp.dest('dist/assets/img'))
+        .pipe($.notify({
             message: 'Images task complete'
         }));
 });
 
 
-// // 雪碧图
-// gulp.task('spritesmith', function(){
-//     gulp.src('src/*.png')
-//         .pipe(spritesmith({
-//             imgName: 'sprite.png',     // 生成的图片
-//             cssName: 'sprite.css',     // 生成的css
-//             padding: 10,               // 图标间的距离
-//             algorithm: 'binary-tree'   // 图标排序方式
-//         }))
-//         .pipe(gulp.dest('dist/assets/images'));
-// });
-
-
 gulp.task('clean', function(cb) {
-    $.del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img'], cb);
+    return del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img'], cb);
 });
 
 
-// gulp serve
+
 gulp.task('watch', ['sass'], function () {
     browserSync.init({
         server: '.'
     });
-    // watch
+
     gulp.watch('src/styles/**/*.scss', ['styles']);
     gulp.watch('src/scripts/**/*.js', ['scripts']);
     gulp.watch('src/images/**/*', ['images']);
@@ -101,5 +90,6 @@ gulp.task('watch', ['sass'], function () {
 });
 
 
-// gulp
-gulp.task('default', ['watch']);
+gulp.task('default', ['clean'], function(cb) {
+    runSequence(['styles', 'scripts', 'images'], ['watch'], cb);
+});
